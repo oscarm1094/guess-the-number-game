8bcadc8e603341e0919ca638fd312f1dfd97c8a7
oscarm1094 <oscarm1094@hotmail.com>
2022-07-07 07:19:19 -0500

    autowiring a bean using annotations

--- core/src/main/java/com/oscar/GameImpl.java
+++ core/src/main/java/com/oscar/GameImpl.java
@@ -2,6 +2,7 @@ package com.oscar;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -11,6 +12,7 @@ public class GameImpl implements Game {
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     // == fields ==
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 10;
     private int number;
@@ -45,11 +47,6 @@ public class GameImpl implements Game {
         log.info("In game preDestroy()");
     }
     // == public methods ==
-    // == SETTER INJECTION ==
-    public void setNumberGenerator(NumberGenerator numberGenerator){
-        this.numberGenerator = numberGenerator;
-    }
-
     @Override
     public int getNumber() {
         return number;
